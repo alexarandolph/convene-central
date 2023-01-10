be9f7ba870c5a8266125eeecef0bbece653b4ae6
Alexa Randolph <alexarand7@gmail.com>
2023-01-10 17:08:07 -0500

    updated conference details view

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -9,6 +9,18 @@ window.addEventListener('DOMContentLoaded', async () => {
         // Figure out what to do when the response is bad
       } else {
         const data = await response.json();
+
+        const conference = data.conferences[0];
+        const nameTag = document.querySelector('.card-title');
+        nameTag.innerHTML = conference.name;
+
+        const detailUrl = `http://localhost:8000${conference.href}`;
+        const detailResponse = await fetch(detailUrl);
+        if (detailResponse.ok) {
+          const details = await detailResponse.json();
+          console.log(details);
+        }
+
       }
     } catch (e) {
       // Figure out what to do if an error is raised
