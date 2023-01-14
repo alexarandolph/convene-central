4ee6338a461859d7ec30884769a37c1bc456f49a
Alexa Randolph <alexarand7@gmail.com>
2023-01-14 17:05:26 -0500

    saving progress on presentation form

--- ghi/js/login.js
+++ ghi/js/login.js
@@ -1,22 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('login-form');
-    form.addEventListener('submit', async event => {
-      event.preventDefault();
+  const form = document.getElementById('login-form');
+  form.addEventListener('submit', async event => {
+    event.preventDefault();
 
-
-      const fetchOptions = {
-        credentials: 'include',
-        method: 'post',
-        body: new FormData(form),
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      };
-      const url = 'http://localhost:8000/login/';
-      const response = await fetch(url, fetchOptions);
-      if (response.ok) {
-        window.location.href = '/';
-      } else {
-        console.error(response);
-      }
-    })});
+    // const data = Object.fromEntries(new FormData(form))
+    const fetchOptions = {
+      method: 'post',
+      body: new FormData(form),
+      credentials: 'include',
+      // headers: {
+      //   'Content-Type': 'application/json',
+      // }
+    };
+    const url = 'http://localhost:8000/login/';
+    const response = await fetch(url, fetchOptions);
+    if (response.ok) {
+      window.location.href = '/';
+    } else {
+      console.error(response);
+    }
+  });
+});
