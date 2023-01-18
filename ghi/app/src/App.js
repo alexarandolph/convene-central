a0a6bfc24678d0828aa1f4e916ded32ab38ccf7e
Alexa Randolph <alexarand7@gmail.com>
2023-01-18 13:13:01 -0500

    added ConferenceForm and AttendConference form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,17 +1,24 @@
 import Nav from './Nav';
 import AttendeesList from './AttendeesList';
 import LocationForm from './LocationForm';
-
+import ConferenceForm from './ConferenceForm';
+import PresentationForm from './PresentationForm';
+import AttendConferenceForm from './AttendConferenceForm';
 
 function App(props) {
+  if (props.attendees === undefined) {
+    return null;
+  }
   return (
     <>
-    <Nav className="Navbar" />
-
-    <div className="container">
-      <LocationForm />
-    {/* <AttendeesList attendees={props.attendees} /> */}
-    </div>
+      <Nav />
+      <div className="container">
+        <LocationForm />
+        {/* <ConferenceForm /> */}
+        {/* <AttendConferenceForm /> */}
+        {/* <PresentationForm /> */}
+        {/* <AttendeesList attendees={props.attendees} /> */}
+      </div>
     </>
   );
 }
